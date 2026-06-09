bfe61d4c6af77cd58d077f1019de2e5f62f8ab25
ihab Al Ubaidi <www.ihabau@gmail.com>
2026-06-09 11:48:46 +0200

    exercise 1 with a class call

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -3,7 +3,7 @@ package se.lexicon;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+    static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         IO.println(String.format("Hello and welcome!"));
@@ -13,5 +13,18 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             IO.println("i = " + i);
         }
+        IO.println("Hello Java!");
+
+        // comments like this same as javascript
+        // calling class to print exercise 1
+        ProfileCard.profileCardClass.printCard("jorden", 44, "gothenborg");
+        ProfileCard.profileCardClass.printCard("Sam", 15, "Malmo");
+
+        // because PrifileCard.main have String[] args in its parameter list we need to pass an empty arg string[]
+        // under is 2 ways to call main with an empty string array
+        String[] emptyArgs = new String[0]; //create an empty string array
+        ProfileCard.main(emptyArgs); // call with main with the empty string array
+        ProfileCard.main(new String[]{});   // call main  without assigning an array beforehand
+
     }
 }
